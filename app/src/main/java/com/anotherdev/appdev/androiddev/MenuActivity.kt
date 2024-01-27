29bb47288e9308c2d3e741c0441b8b16f41974c5
Anothher <sepulvedaluis234@gmail.com>
2024-01-27 18:59:17 -0500

    Update

--- app/src/main/java/com/anotherdev/appdev/androiddev/MenuActivity.kt
+++ app/src/main/java/com/anotherdev/appdev/androiddev/MenuActivity.kt
@@ -1,12 +1,31 @@
 package com.anotherdev.appdev.androiddev
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.anotherdev.appdev.R
+import com.anotherdev.appdev.androiddev.firstapp.FirstAppActivity
+import imccalculator.ImcCalculatorActivity
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+        val btnSaludApp = findViewById<Button>(R.id.btnSaludApp)
+        val btnImcApp = findViewById<Button>(R.id.IMCApp)
+        btnSaludApp.setOnClickListener{navigateToSalidApp()}
+        btnImcApp.setOnClickListener { navigateToImcApp() }
+
+    }
+
+    private fun navigateToImcApp() {
+    val intent = Intent(this, ImcCalculatorActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun navigateToSalidApp(){
+        val intent = Intent(this, FirstAppActivity::class.java)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/imccalculator/ImcCalculatorActivity.kt
@@ -0,0 +1,14 @@
+package imccalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.anotherdev.appdev.R
+
+class ImcCalculatorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_imc_calculator)
+
+
+    }
+}
